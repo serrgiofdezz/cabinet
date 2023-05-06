37b0c5a6dabae1c1d6ba2dce895bf3429ebdb29c
Sergio Fdez <sergio@bbjprojek.org>
2023-05-06 12:13:03 +0200

    cabinet: utc: Use 24h format
    Signed-off-by: Sergio Fdez <sergio@bbjprojek.org>

--- assets/utc.js
+++ assets/utc.js
@@ -1,7 +1,12 @@
 function updateTime() {
   var now = new Date();
-  var utcString = now.toUTCString();
-  var time = utcString.slice(-11, -4);
+  var hours = now.getUTCHours();
+  var minutes = now.getUTCMinutes();
+  var seconds = now.getUTCSeconds();
+
+  // Format the hours, minutes, and seconds as a string in 24-hour format
+  var time = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
+
   document.getElementById('clock').innerHTML = time;
 }
 
