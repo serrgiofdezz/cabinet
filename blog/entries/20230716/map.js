81ffff3cb7d17290b2487f2ebbbaa0283616303d
Sergio Fdez <sergio@bbjprojek.org>
2023-07-16 18:16:24 +0200

    wiki: blog: Update blog entries and generate design
    * Add entries
    * Update blog entries style (temp)
    
    Signed-off-by: Sergio Fdez <sergio@bbjprojek.org>

---
+++ blog/entries/20230716/map.js
@@ -0,0 +1,7 @@
+
+const map = L.map('map').setView([40.2990, -3.7529], 16);
+
+	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
