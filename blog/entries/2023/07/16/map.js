aea776f93be176a2e982921b84de305b2323bc06
Sergio Fdez <sergio@bbjprojek.org>
2023-10-11 19:36:09 +0200

    cabinet: blog: Revert back to OSM default carto
    Signed-off-by: Sergio Fdez <sergio@bbjprojek.org>

--- blog/entries/2023/07/16/map.js
+++ blog/entries/2023/07/16/map.js
@@ -1,15 +1,6 @@
-// center of the map
-var center = [40.2990, -3.7529];
+const map = L.map('map').setView([40.2941, -3.7491], 15);
 
-// Create the map
-var map = L.map('map').setView(center, 17);
-
-// Set up the OSM layer
-L.tileLayer(
-  'https://tile.tracestrack.com/_/{z}/{x}/{y}.png?key=54e3f4571ed5c91b599db36db631cfdc', {
-    maxZoom: 19,
-    attribution: 'Data: © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>, <a href="https://www.tracestrack.com/">Tracestrack</a>'
-  }).addTo(map);
-
-// add a marker in the given location
-// L.marker(center).addTo(map);
+	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
