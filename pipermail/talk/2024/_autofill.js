6aaaf8f2537bd925af28ead45aca98c303e0e723
Sergio Fdez <sergio@bbjprojek.org>
2024-01-26 16:09:57 +0100

    cabinet: pipermail: Update scripts
    * Messages are now counted automatically
    
    Signed-off-by: Sergio Fdez <sergio@bbjprojek.org>

--- pipermail/talk/2024/_autofill.js
+++ pipermail/talk/2024/_autofill.js
@@ -1,19 +1,16 @@
 // Copyright C 2024 The BBJProjeK Organization
 
-// Function to update content
 function updateContent() {
-  // Get elements by id
+  var monthElement = document.getElementById('month');
   var fromDateElement = document.getElementById('from_date');
   var toDateElement = document.getElementById('to_date');
-  var messagesElement = document.getElementById('messages');
   var pipermailRevisionElement = document.getElementById('pipermail_revision');
 
-  // Update content
+  // Content to edit
+  monthElement.textContent = "January 2024";
   fromDateElement.textContent = "Tue Jan 2 16:58:55 UTC 2024";
   toDateElement.textContent = "Fri Jan 26 09:16:01 UTC 2024";
-  messagesElement.textContent = "5";
-  pipermailRevisionElement.textContent = "0.11a";
+  pipermailRevisionElement.textContent = "0.10a";
 }
 
-// Call the function to update content
 updateContent();
