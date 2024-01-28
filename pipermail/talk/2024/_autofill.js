117458e757cf98e5567a760d6caef4fe1c19eff4
Sergio Fdez <sergio@bbjprojek.org>
2024-01-28 15:05:00 +0100

    cabinet: pipermail: Update January Tickets
    Signed-off-by: Sergio Fdez <sergio@bbjprojek.org>

--- pipermail/talk/2024/_autofill.js
+++ pipermail/talk/2024/_autofill.js
@@ -4,12 +4,13 @@ function updateContent() {
   var monthElement = document.getElementById('month');
   var fromDateElement = document.getElementById('from_date');
   var toDateElement = document.getElementById('to_date');
+  // var toDateElement = document.getElementById('to_date_');
   var pipermailRevisionElement = document.getElementById('pipermail_revision');
 
   // Content to edit
   monthElement.textContent = "January 2024";
   fromDateElement.textContent = "Tue Jan 2 16:58:55 UTC 2024";
-  toDateElement.textContent = "Fri Jan 26 09:16:01 UTC 2024";
+  toDateElement.textContent = "Sun Jan 28 17:00:59 UTC 2024";
   pipermailRevisionElement.textContent = "0.10a";
 }
 
