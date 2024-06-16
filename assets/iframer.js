c2feebead3a0680ee5fb7efec1a8ddb343d3db87
Sergio Fdez <sergio@bbjprojek.org>
2024-06-16 18:39:58 +0200

    treewide: Hide iframes as default for more privacy
    Signed-off-by: Sergio Fdez <sergio@bbjprojek.org>

---
+++ assets/iframer.js
@@ -0,0 +1,5 @@
+document.getElementById('showiframe').addEventListener('click', function() {
+  var iframeContainer = document.getElementById('iframeContainer');
+  iframeContainer.style.display = 'block';
+  this.style.display = 'none';
+});
