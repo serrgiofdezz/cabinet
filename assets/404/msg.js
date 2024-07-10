475a33e255211f0343e5e0253aaf2cb0e0c59980
Sergio Fdez <sergiorafez@gmail.com>
2024-07-10 18:04:09 +0200

    404: Update things up

--- assets/404/msg.js
+++ assets/404/msg.js
@@ -1,46 +1,36 @@
 const phrases = [
-  "It's too late to apologize",
+  "Too late to apologize",
   "Please be patient with me",
-  "U look so proud standing there, with a frown and a cigarette",
-  "No teardrops on the dance floor, not tonight",
-  "I can't live, with or without you",
-  "So much to do and not enough time...",
+  "Pour your glass of wine",
+  "So much to do and not enough time",
   "This girl been stressin' me"
 ];
 
 const randomIndex = Math.floor(Math.random() * phrases.length);
 const selectedPhrase = phrases[randomIndex];
 
-// Select the .header-title element
 const title = document.querySelector('.header-title');
-
-// Set the text content of the .header-title element to the selected phrase
 title.textContent = selectedPhrase;
 
-// Split the text into words
 const words = selectedPhrase.split(' ');
-
-// Clear the existing content of .header-title
 title.innerHTML = '';
 
-// Rebuild the text with span elements for each letter
+// Rebuilding text!
 words.forEach((word, wordIndex) => {
   if (wordIndex > 0) {
     title.appendChild(document.createTextNode(' '));
   }
-
   const wordElement = document.createElement('span');
   wordElement.textContent = word;
   title.appendChild(wordElement);
 });
 
-// Select all the span elements within .header-title
 const letters = title.querySelectorAll('span');
 
-// Apply animation to each letter
+// Let's animate it
 letters.forEach((letter, index) => {
   setTimeout(() => {
-    letter.style.animationDelay = `${0.09 * index}s`;
-    letter.style.animationDuration = '0.5s';
+    letter.style.animationDelay = `${0.08 * index}s`;
+    letter.style.animationDuration = '0.6s';
   }, 0);
 });
