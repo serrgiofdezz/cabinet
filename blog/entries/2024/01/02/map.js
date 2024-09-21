5e393a1e032e88af2fda65428e411f5ef6ccc00f
Sergio Fdez <sergio@bbjprojek.org>
2024-09-21 12:05:37 +0200

    blog: Remove old posts
    Signed-off-by: Sergio Fdez <sergio@bbjprojek.org>

--- blog/entries/2024/01/02/map.js
+++
@@ -1,6 +0,0 @@
-const map = L.map('map').setView([40.25796, -3.69863], 16);
-
-	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 19,
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(map);
