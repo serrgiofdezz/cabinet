9e30bf2c38a2a58454e17655ce599c76f37a481c
Sergio Fdez <sergio@bbjprojek.org>
2024-10-08 15:37:12 +0200

    xaspos: Generator js fixup
    Signed-off-by: Sergio Fdez <sergio@bbjprojek.org>

--- xaspos/generator/assets/replacer.js
+++ xaspos/generator/assets/replacer.js
@@ -3,9 +3,7 @@
 //
 
 const qs = document.querySelector.bind(document);
-
 const copied = document.querySelector('#copied');
-
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 function ifEmpty(input, fb) {
@@ -13,32 +11,31 @@ function ifEmpty(input, fb) {
 }
 
 function doReplace() {
-    const title = qs('#title').value;
-    const previous_title = qs('#previous_title').value;
-    const date = qs('#date').value;
-		const author = qs('#author').value;
-		const content = qs('#content').value;
-    const content_link = ifEmpty(qs('#content_link').value, '/');
+  const title = qs('#title').value;
+  const previous_title = qs('#previous_title').value;
+  const date = qs('#date').value;
+  const author = qs('#author').value;
+  const content = qs('#content').value;
+  const content_link = ifEmpty(qs('#content_link').value, '/');
 
-    qs('#output').value =
-        template.replace(/##TITLE##/g, title)
-				.replace(/##PREVIOUS_TITLE##/g, previous_title)
-        .replace(/##DATE##/g, date)
-				.replace(/##AUTHOR_EMAIL##/g, author)
-				.replace(/##CONTENT##/g, content)
-        .replace(/##CONTENT_LINK##/g, content_link)
-        .replace(/@@/g, '\n')
+  qs('#output').value =
+  template.replace(/##TITLE##/g, title)
+  .replace(/##PREVIOUS_TITLE##/g, previous_title)
+  .replace(/##DATE##/g, date)
+  .replace(/##AUTHOR_EMAIL##/g, author)
+  .replace(/##CONTENT##/g, content)
+  .replace(/##CONTENT_LINK##/g, content_link)
+  .replace(/@@/g, '\n')
 }
 
 qs('#form').addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    doReplace();
-    copied.style.opacity = 1;
-    document.getElementById('output').select();
-    document.execCommand('copy');
-    document.getElementById('copied').innerHTML = 'Copied';
-    delay(2000).then(() => {
-        copied.style.opacity = 0;
-    });
+  event.preventDefault();
+  doReplace();
+  copied.style.opacity = 1;
+  document.getElementById('output').select();
+  document.execCommand('copy');
+  document.getElementById('copied').innerHTML = 'Copied';
+  delay(2000).then(() => {
+    copied.style.opacity = 0;
+  });
 });
